705bb4cf15b625bdb2d4bc1d8144e8aeda4c6c7b
Joe Jackson <ph8tel@gmail.com>
2022-04-04 14:19:06 -0700

    added dynamic port

--- server.py
+++ server.py
@@ -48,41 +48,96 @@ async def index(request):
         text="""
 <html>
         <head>
-            <title>RTCBot: Remote Video</title>
+            <title>Joes Yardbot</title>
             <script src="/rtcbot.js"></script>
+            <style>
+                body {
+                  background-color:#E7E9EB;
+                }
+                #myDIV {
+                  width:100%;
+                  position:absolute;
+                  background-color:#FFFFFF;
+                }
+                #myDIV div{
+                  /* width:100px;
+                  height:100px; */
+                  width:100%;
+
+                  position:absolute;
+                  /* background-color:yellow; */
+                  border:1px solid;
+                  opacity:0.5;
+                  margin:auto;
+                }
+                #myBox {
+                  position:absolute;
+                  opacity:1!important;
+                  z-index: 0;
+                  width:100%;
+                }
+                .arrow {
+                  width: 30px;
+                  height: 30px;
+                  border: solid black;
+                  border-width: 0 3px 3px 0;
+                  display: inline-block;
+                  padding: 30px;
+                  position:absolute;
+                  text-color: white;
+                }
+                
+                .right {
+                  transform: rotate(-45deg);
+                  -webkit-transform: rotate(-45deg);
+                }
+                
+                .left {
+                  transform: rotate(135deg);
+                  -webkit-transform: rotate(135deg);
+                }
+                
+                .up {
+                  transform: rotate(-135deg);
+                  -webkit-transform: rotate(-135deg);
+                }
+                
+                .down {
+                  transform: rotate(45deg);
+                  -webkit-transform: rotate(45deg);
+                }
+                
+                </style>
         </head>
-        <body style="text-align: center;padding-top: 30px;">
-            <video autoplay playsinline controls width = "100%"></video> <audio autoplay></audio>
-            <p>
-            </p>
-            <button class="ctrl" id="forward">FORWARD</button>
-            <button class="ctrl" id="back">BACK</button>
-            <button class="ctrl" id="left">LEFT</button>
-            <button class="ctrl" id="right">RIGHT</button>
-            <input type="text" id="cmd" name = "cmd">
+        <body>
+            <div id="myDIV">
+                <div id="myBox">
+            <video autoplay playsinline controls width = "80%"></video> <audio autoplay></audio>
+                </div>
+            
+                    <i id='forward' class="arrow up ctrl" style="top:40px;left:85px;z-index:1;"></i> 
+                    <i id='left' class="arrow left ctrl" style="top:100px;left:30px;z-index:1;"></i> 
+                    <i id='right'class="arrow right ctrl" style="top:100px;left:180px;z-index:1;"></i> 
+                    <i id ="back" class="arrow down ctrl" style="top:220px;left:85px;z-index:1;"></i>  
+            </div>
+
+
             <script>
                 var conn = new rtcbot.RTCConnection();
-                const r = document.getElementById("cmd")
                 var sendControlSignal = (cmd) => {
                     cmd.preventDefault();
                     conn.put_nowait(cmd.target.id)
                     }
                 var sendStopSignal = (cmd) => conn.put_nowait("stop")
-                
-                var parrot = (e) => {
-                    console.log(e.target.id)
-                }
+
                 var buttons = document.getElementsByClassName("ctrl")
-                console.log(buttons)
                 for (let i = 0; i < buttons.length; i++){
-                    console.log("adding")
                     buttons[i].addEventListener("mousedown", sendControlSignal)
                     buttons[i].addEventListener("mouseup", sendStopSignal)
                     buttons[i].addEventListener("touchstart", sendControlSignal )
                     buttons[i].addEventListener("touchend", sendStopSignal )
-
                 }
-console.log()
+
                 conn.video.subscribe(function(stream) {
                     document.querySelector("video").srcObject = stream;
                 });
@@ -122,6 +177,6 @@ app = web.Application()
 app.add_routes(routes)
 app.on_shutdown.append(cleanup)
 
-#web.run_app(app, path="0.0.0.0", port=os.environ["PORT"])
-web.run_app(app)
+web.run_app(app, path="0.0.0.0", port=os.environ["PORT"])
+#web.run_app(app)
 
