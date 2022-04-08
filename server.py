3ee6ca4a6cd34a27dc9cff6345f6719b12b9e0b6
Joe Jackson <ph8tel@gmail.com>
2022-04-07 23:16:11 -0700

    added truck routes

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ from rtcbot import Websocket, getRTCBotJS
 
 # Websocket connection to the robot
 ws = None
-
+# Truck Camera Socket
+ts = None
 
 # Serve the RTCBot javascript library at /rtcbot.js
 @routes.get("/rtcbot.js")
@@ -30,7 +31,28 @@ async def connect(request):
 
     return web.json_response(robotResponse)
 
+@routes.post("/connect-truck")
+async def connect(request):
+    global ts
+    if ts is None:
+        raise web.HTTPInternalServerError("There is no robot connected")
+    clientOffer = await request.json()
 
+    # Send the offer to the robot, and receive its response
+    ts.put_nowait(clientOffer)
+    robotResponse = await ts.get()
+
+    return web.json_response(robotResponse)
+#route for truck
+@routes.get("/ts")
+async def websocket(request):
+    global ts
+    ts = Websocket(request)
+    print("Truck Connected")
+    await ts  # Wait until the websocket closes
+    print("Robot disconnected")
+    return ts.ws
+#route for car
 @routes.get("/ws")
 async def websocket(request):
     global ws
@@ -40,6 +62,132 @@ async def websocket(request):
     print("Robot disconnected")
     return ws.ws
 
+@routes.get("/truck")
+async def index(request):
+    return web.Response(
+        content_type="text/html",
+        text="""
+<html>
+        <head>
+            <title>Joes Yardbot</title>
+            <script src="/rtcbot.js"></script>
+            <style>
+                body {
+                  background-color:#E7E9EB;
+                }
+                #myDIV {
+                  width:100%;
+                  position:absolute;
+                  background-color:#FFFFFF;
+                }
+                #myDIV div{
+                  /* width:100px;
+                  height:100px; */
+                  width:100%;
+
+                  position:absolute;
+                  /* background-color:yellow; */
+                  border:1px solid;
+                  opacity:0.5;
+                  margin:auto;
+                }
+                #myBox {
+                  position:absolute;
+                  opacity:1!important;
+                  z-index: 0;
+                  width:100%;
+                }
+                video{
+                    position: absolute;
+                    z-index: 0;
+                }
+                .arrow {
+                    opacity: 60%;
+                  width: 30px;
+                  height: 30px;
+                  border: solid white;
+                  border-width: 0 3px 3px 0;
+                  display: inline-block;
+                  padding: 30px;
+                  position:absolute;
+                  color: #E7E9EB;
+                }
+                
+                .right {
+                  transform: rotate(-45deg);
+                  -webkit-transform: rotate(-45deg);
+                }
+                
+                .left {
+                  transform: rotate(135deg);
+                  -webkit-transform: rotate(135deg);
+                }
+                
+                .up {
+                  transform: rotate(-135deg);
+                  -webkit-transform: rotate(-135deg);
+                }
+                
+                .down {
+                  transform: rotate(45deg);
+                  -webkit-transform: rotate(45deg);
+                }
+                
+                </style>
+        </head>
+        <body>
+            <div id="myDIV">
+                <div id="myBox">
+            <video autoplay playsinline controls width = "640px" height="480"></video> <audio autoplay></audio>
+                </div>
+            
+                    <i id='forward' class="arrow up ctrl" style="top:40px;left:95px;z-index:1;"></i> 
+                    <i id='left' class="arrow left ctrl" style="top:120px;left:20px;z-index:1;"></i> 
+                    <i id='right'class="arrow right ctrl" style="top:120px;left:170px;z-index:1;"></i> 
+                    <i id ="back" class="arrow down ctrl" style="top:200px;left:95px;z-index:1;"></i>  
+            </div>
+
+
+            <script>
+                var conn = new rtcbot.RTCConnection();
+                var sendControlSignal = (cmd) => {
+                    cmd.preventDefault();
+                    conn.put_nowait(cmd.target.id)
+                    }
+                var sendStopSignal = (cmd) => conn.put_nowait("stop")
+
+                var buttons = document.getElementsByClassName("ctrl")
+                for (let i = 0; i < buttons.length; i++){
+                    buttons[i].addEventListener("mousedown", sendControlSignal)
+                    buttons[i].addEventListener("mouseup", sendStopSignal)
+                    buttons[i].addEventListener("touchstart", sendControlSignal )
+                    buttons[i].addEventListener("touchend", sendStopSignal )
+                }
+
+                conn.video.subscribe(function(stream) {
+                    document.querySelector("video").srcObject = stream;
+                });
+
+                async function connect() {
+                    let offer = await conn.getLocalDescription();
+
+                    // POST the information to /connect
+                    let response = await fetch("/connect-truck", {
+                        method: "POST",
+                        cache: "no-cache",
+                        body: JSON.stringify(offer)
+                    });
+
+                    await conn.setRemoteDescription(await response.json());
+
+                    console.log("Ready!");
+                }
+                connect();
+
+            </script>
+        </body>
+    </html>
+        """,)
 
 @routes.get("/")
 async def index(request):
