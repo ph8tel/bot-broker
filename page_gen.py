e6cc7fc5952e6f5fda38e4cba58f682cf16e015f
Joe Jackson <ph8tel@gmail.com>
2022-09-08 17:42:56 -0600

    changed route definition

--- page_gen.py
+++ page_gen.py
@@ -4,129 +4,101 @@ truck = """
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Joe's Yardbot</title>
+  <title>yardbot</title>
   <script src="/rtcbot.js"></script>
   <style>
-    body {
-      background: #fff;
-      font: 16px/1 Arial, Helvetica, sans-serif;
-      padding: 0;
-      margin: 0;
+    .arrow {
+        opacity: 60%;
+      width: 30px;
+      height: 30px;
+      border: solid white;
+      border-width: 0 3px 3px 0;
+      display: inline-block;
+      padding: 30px;
+      position:absolute;
+      color: #E7E9EB;
     }
-                .arrow {
-                    opacity: 60%;
-                  width: 30px;
-                  height: 30px;
-                  border: solid white;
-                  border-width: 0 3px 3px 0;
-                  display: inline-block;
-                  padding: 30px;
-                  position:absolute;
-                  color: #E7E9EB;
-                }
-                
-                .right {
-                  transform: rotate(-45deg);
-                  -webkit-transform: rotate(-45deg);
-                }
-                
-                .left {
-                  transform: rotate(135deg);
-                  -webkit-transform: rotate(135deg);
-                }
-                
-                .up {
-                  transform: rotate(-135deg);
-                  -webkit-transform: rotate(-135deg);
-                }
-                
-                .down {
-                  transform: rotate(45deg);
-                  -webkit-transform: rotate(45deg);
-                }
-    .grid-container {
-      display: grid;
-      grid-template-columns: auto auto;
-      grid-template-rows: auto auto;
-      grid-column-gap: 10px;
-      grid-row-gap: 10px;
-      background: #fff;
-      padding: 5px;
+    
+    .right {
+      transform: rotate(-90deg);
+      -webkit-transform: rotate(-90deg);
+      left: 20px;
     }
+    
+    .left {
+      transform: rotate(90deg);
+      -webkit-transform: rotate(90deg);
+    }
+    
 
-    .grid-item {
-      background: rgba(255, 255, 255, 0.8);
-      border: 1px solid rgba(0, 0, 0, 0.8);
-      padding: 5px;
-      font-size: 12px;
-      text-align: center;
+    .down {
+      transform: rotate(180deg);
+      -webkit-transform: rotate(180deg);
+    }
+   .centered {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+    body {
+      background-image: url("https://www.rv.com/wp-content/uploads/2020/12/IMG_1677.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
     }
   </style>
 </head>
 
 <body>
-
-<h1>Joe's Yardbot</h1>
-
-<div class="grid-container">
-  <div class="grid-item">            
-    <div id="myDIV">
-        <div id="myBox">
-         <video autoplay playsinline controls width = "640px" height="480"></video> <audio autoplay></audio>
-        </div>
+    <div class="centered">
+      <img id="left" width="100px" src="https://toppng.com/uploads/preview/wind-direction-png-clipart-library-wind-direction-icon-115629307179jemtlpetx.png" class="ctrl right" />
+      <img id="up" width="100px" src="https://toppng.com/uploads/preview/wind-direction-png-clipart-library-wind-direction-icon-115629307179jemtlpetx.png" class="ctrl up" />
+      <img id="down" width="100px" src="https://toppng.com/uploads/preview/wind-direction-png-clipart-library-wind-direction-icon-115629307179jemtlpetx.png" class="ctrl down" />
+      <img id="right" width="100px" src="https://toppng.com/uploads/preview/wind-direction-png-clipart-library-wind-direction-icon-115629307179jemtlpetx.png" class="ctrl left" />
+      <img width="218px" height="100px" src="https://p.kindpng.com/picc/s/107-1078619_stop-sign-icon-png-stop-sign-black-and.png" />      <video autoplay playsinline controls width = "640px" height="480"></video>    
+      <button class="ctrl" id="off">off</button>  
     </div>
-   </div>  
-    <div class="grid-item">
-    <div style="background-color: black; height: 300px; padding-top: 20px">
-        <i id='forward' class="arrow up ctrl" style="padding-top: 20px;"></i> 
-        <i id='left' class="arrow left ctrl" ></i> 
-        <i id='right'class="arrow right ctrl" ></i> 
-        <i id ="back" class="arrow down ctrl" ></i>
-        
-    </div>
-    <button class="ctrl" id="off">off</button>  
-  </div>
+  
+  <script>
+      var conn = new rtcbot.RTCConnection();
+      var sendControlSignal = (cmd) => {
+          cmd.preventDefault();
+          conn.put_nowait(cmd.target.id)
+          }
+      var sendStopSignal = (cmd) => conn.put_nowait("stop")
 
-</div>
-           <script>
-                var conn = new rtcbot.RTCConnection();
-                var sendControlSignal = (cmd) => {
-                    cmd.preventDefault();
-                    conn.put_nowait(cmd.target.id)
-                    }
-                var sendStopSignal = (cmd) => conn.put_nowait("stop")
+      var buttons = document.getElementsByClassName("ctrl")
+      for (let i = 0; i < buttons.length; i++){
+          buttons[i].addEventListener("mousedown", sendControlSignal)
+          buttons[i].addEventListener("mouseup", sendStopSignal)
+          buttons[i].addEventListener("touchstart", sendControlSignal )
+          if (buttons[i] !== 'off') {
+            buttons[i].addEventListener("touchend", sendStopSignal )
+          }
+      }
 
-                var buttons = document.getElementsByClassName("ctrl")
-                for (let i = 0; i < buttons.length; i++){
-                    buttons[i].addEventListener("mousedown", sendControlSignal)
-                    buttons[i].addEventListener("mouseup", sendStopSignal)
-                    buttons[i].addEventListener("touchstart", sendControlSignal )
-                    if  (buttons[i].id !== 'off') {
-                      buttons[i].addEventListener("touchend", sendStopSignal )
-                
-                    }
-                  }
-                conn.video.subscribe(function(stream) {
-                    document.querySelector("video").srcObject = stream;
-                });
+      conn.video.subscribe(function(stream) {
+          document.querySelector("video").srcObject = stream;
+      });
 
-                async function connect() {
-                    let offer = await conn.getLocalDescription();
+      async function connect() {
+          let offer = await conn.getLocalDescription();
 
-                    // POST the information to /connect
-                    let response = await fetch("/connect-truck", {
-                        method: "POST",
-                        cache: "no-cache",
-                        body: JSON.stringify(offer)
-                    });
+          // POST the information to /connect
+          let response = await fetch("/connect-truck", {
+              method: "POST",
+              cache: "no-cache",
+              body: JSON.stringify(offer)
+          });
 
-                    await conn.setRemoteDescription(await response.json());
+          await conn.setRemoteDescription(await response.json());
 
-                    console.log("Ready!");
-                }
-                connect();
+          console.log("Ready!");
+      }
+      connect();
 
-            </script>
+  </script>
+
 </body>
 </html>
 """
